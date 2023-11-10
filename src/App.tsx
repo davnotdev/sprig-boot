6d20b45092f56893d2f83c26010d24b8b894d65d
dav <usr21p1@gmail.com>
2023-11-10 11:53:23 -0800

    Add fork me

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import { Show, createSignal } from "solid-js";
+//  @ts-ignore
+import forkMe from "fork-me-github";
 
 import Generate from "./Generate.tsx";
 import Selection, { SelectionChoice } from "./Selection.tsx";
@@ -13,24 +15,23 @@ function App() {
   let [state, setState] = createSignal(null as SelectionType);
   let [generateData, setGenerateData] = createSignal(null as GenerateData);
 
+  forkMe("https://github.com/davnotdev/sprig-boot");
+
   return (
-      <div class="flex flex-col space-y-5">
-        <h1>Sprig Boot</h1>
-        <h2>Load multiple games on your little sprig!</h2>
-        <Selection setState={setState} setGenerateData={setGenerateData} />
-        <Show when={state() == "choice"}>
-          <Choice
-            generateData={generateData}
-            setGenerateData={setGenerateData}
-          />
-        </Show>
-        <Show when={state() == "surprise"}>
-          <Surprise setGenerateData={setGenerateData} />
-        </Show>
-        <Show when={generateData() != null}>
-          <Generate generateData={generateData} />
-        </Show>
-      </div>
+    <div class="flex flex-col space-y-5">
+      <h1>Sprig Boot</h1>
+      <h2>Load multiple games on your little sprig!</h2>
+      <Selection setState={setState} setGenerateData={setGenerateData} />
+      <Show when={state() == "choice"}>
+        <Choice generateData={generateData} setGenerateData={setGenerateData} />
+      </Show>
+      <Show when={state() == "surprise"}>
+        <Surprise setGenerateData={setGenerateData} />
+      </Show>
+      <Show when={generateData() != null}>
+        <Generate generateData={generateData} />
+      </Show>
+    </div>
   );
 }
 
