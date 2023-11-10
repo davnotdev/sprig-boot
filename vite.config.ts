8096c8ac6ce3a7acb2a2160ab82bb264454c45c3
dav <usr21p1@gmail.com>
2023-11-09 22:12:31 -0800

    Set Correct Base for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
 
 export default defineConfig({
+  base: "https://davnotdev.github.io/sprig-boot/",
   plugins: [solid()],
-})
+});
